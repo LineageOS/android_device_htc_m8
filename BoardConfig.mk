c74e1c7a6f71d82ab1dd263336aa2ab5084cee23
Bruno Martins <bgcngm@gmail.com>
2017-06-16 14:35:54 +0100

    m8: Abandon TARGET_UNIFIED_DEVICE flag
     * Override the unified properties using vendor init
    
    Change-Id: Id04671dfa32c5480000dcbe4e2d9aae3a81402b9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
+#               2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -56,7 +57,6 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 13153337344
 # Vendor Init
 TARGET_INIT_VENDOR_LIB := libinit_m8
 TARGET_RECOVERY_DEVICE_MODULES := libinit_m8
-TARGET_UNIFIED_DEVICE := true
 
 # Inherit from the proprietary version
 -include vendor/htc/m8/BoardConfigVendor.mk
--- init/init_m8.cpp
+++ init/init_m8.cpp
@@ -88,11 +88,11 @@ void vendor_load_properties()
         /* m8vzw (m8wl) */
         common_properties();
         cdma_properties("0", "10");
-        property_set("ro.product.model", "m8wl");
-        property_set("ro.build.fingerprint", "htc/HTCOneM8vzw/htc_m8wl:6.0/MRA58K/708002.3:user/release-keys");
-        property_set("ro.build.description", "6.21.605.3 CL708002 release-keys");
-        property_set("ro.product.device", "htc_m8wl");
-        property_set("ro.build.product", "htc_m8wl");
+        property_override("ro.product.model", "m8wl");
+        property_override("ro.build.fingerprint", "htc/HTCOneM8vzw/htc_m8wl:6.0/MRA58K/708002.3:user/release-keys");
+        property_override("ro.build.description", "6.21.605.3 CL708002 release-keys");
+        property_override("ro.product.device", "htc_m8wl");
+        property_override("ro.build.product", "htc_m8wl");
         property_set("ro.ril.vzw.feature", "1");
         property_set("ro.ril.oem.ecclist", "911,*911,#911");
         property_set("ro.ril.enable.a52", "0");
@@ -120,11 +120,11 @@ void vendor_load_properties()
         /* m8spr (m8whl) */
         common_properties();
         cdma_properties("1", "8");
-        property_set("ro.product.model", "m8whl");
-        property_set("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:6.0/MRA58K/682910.3:user/release-keys");
-        property_set("ro.build.description", "6.20.651.3 CL682910 release-keys");
-        property_set("ro.product.device", "htc_m8whl");
-        property_set("ro.build.product", "htc_m8whl");
+        property_override("ro.product.model", "m8whl");
+        property_override("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:6.0/MRA58K/682910.3:user/release-keys");
+        property_override("ro.build.description", "6.20.651.3 CL682910 release-keys");
+        property_override("ro.product.device", "htc_m8whl");
+        property_override("ro.build.product", "htc_m8whl");
         property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420,23594,27202,27205");
         property_set("ro.ril.oem.ecclist", "911");
         property_set("ro.ril.set.mtusize", "1422");
@@ -139,10 +139,11 @@ void vendor_load_properties()
         /* m8 */
         common_properties();
         gsm_properties("9");
-        property_set("ro.product.model", "m8");
-        property_set("ro.build.fingerprint", "htc/m8_google/htc_m8:6.0/MRA58K.H6/648564:user/release-keys");
-        property_set("ro.build.description", "5.07.1700.6 CL648564 release-keys");
-        property_set("ro.product.device", "htc_m8");
+        property_override("ro.product.model", "m8");
+        property_override("ro.build.fingerprint", "htc/m8_google/htc_m8:6.0/MRA58K.H6/648564:user/release-keys");
+        property_override("ro.build.description", "5.07.1700.6 CL648564 release-keys");
+        property_override("ro.product.device", "htc_m8");
+        property_override("ro.build.product", "htc_m8");
     }
 
     device = property_get("ro.product.device");
