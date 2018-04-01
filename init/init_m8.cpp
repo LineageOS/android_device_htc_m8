4f7f2546f0a41b4904cd2d7e064ae7cde3166bed
Bruno Martins <bgcngm@gmail.com>
2018-04-01 11:20:14 +0100

    m8: Also override certain vendor build properties
    Change-Id: I8b5e5128c291c97d23705a8427b58446b7a0cb75

--- init/init_m8.cpp
+++ init/init_m8.cpp
@@ -97,10 +97,10 @@ void vendor_load_properties()
         /* m8vzw (m8wl) */
         common_properties();
         cdma_properties("0", "10");
-        property_override("ro.product.model", "m8wl");
-        property_override("ro.build.fingerprint", "htc/HTCOneM8vzw/htc_m8wl:6.0/MRA58K/708002.3:user/release-keys");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "m8wl");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "htc/HTCOneM8vzw/htc_m8wl:6.0/MRA58K/708002.3:user/release-keys");
         property_override("ro.build.description", "6.21.605.3 CL708002 release-keys");
-        property_override("ro.product.device", "htc_m8wl");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "htc_m8wl");
         property_override("ro.build.product", "htc_m8wl");
         property_override("ro.com.google.clientidbase", "android-verizon");
         property_set("ro.ril.vzw.feature", "1");
@@ -130,10 +130,10 @@ void vendor_load_properties()
         /* m8spr (m8whl) */
         common_properties();
         cdma_properties("1", "8");
-        property_override("ro.product.model", "m8whl");
-        property_override("ro.build.fingerprint", "htc/sprint_wwe/htc_m8whl:6.0/MRA58K/682910.3:user/release-keys");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "m8whl");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "htc/sprint_wwe/htc_m8whl:6.0/MRA58K/682910.3:user/release-keys");
         property_override("ro.build.description", "6.20.651.3 CL682910 release-keys");
-        property_override("ro.product.device", "htc_m8whl");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "htc_m8whl");
         property_override("ro.build.product", "htc_m8whl");
         property_set("ro.ril.disable.fd.plmn.prefix", "23402,23410,23411,23420,23594,27202,27205");
         property_set("ro.ril.oem.ecclist", "911");
@@ -148,10 +148,10 @@ void vendor_load_properties()
         /* m8 */
         common_properties();
         gsm_properties("9");
-        property_override("ro.product.model", "m8");
-        property_override("ro.build.fingerprint", "htc/m8_google/htc_m8:6.0/MRA58K.H6/648564:user/release-keys");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "m8");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "htc/m8_google/htc_m8:6.0/MRA58K.H6/648564:user/release-keys");
         property_override("ro.build.description", "5.07.1700.6 CL648564 release-keys");
-        property_override("ro.product.device", "htc_m8");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "htc_m8");
         property_override("ro.build.product", "htc_m8");
     }
 
