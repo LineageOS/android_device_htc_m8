13d81eff1f7443a3876522aefa7294593735ea9d
Bruno Martins <bgcngm@gmail.com>
2018-08-10 14:21:40 +0100

    m8: Consider lineage_m8 product via the AOSP way
    Change-Id: I5b8cee6fc2b8c9bd5ecc684bd7864671e721e59f

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2015-2016 The CyanogenMod Project
-#               2017 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,17 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Inherit from m8 device
-$(call inherit-product, device/htc/m8/device.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_m8
-PRODUCT_DEVICE := m8
-PRODUCT_BRAND := htc
-PRODUCT_MODEL := m8
-PRODUCT_MANUFACTURER := HTC
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_m8.mk
